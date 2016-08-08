8c28a47acfc5ef540a017abcd786cf815591b163
chen <chensandiego@gmail.com>
2016-08-08 17:16:39 +0800

    final

---
+++ main/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class SearchForm(forms.Form):
+	name=forms.CharField(required=False)
+	min_price=forms.IntegerField(required=False,label="Minumum Price")
+	max_price=forms.IntegerField(required=False,label="Maximum Price")
